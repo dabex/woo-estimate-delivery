a34f0455024686657f088f334f4bfb2f36d58d7e
Daniel Florea <dabexonlinemarket@gmail.com>
2020-08-07 23:48:17 +0300

    Update class-estimate-delivery-admin.php

--- admin/class-estimate-delivery-admin.php
+++ admin/class-estimate-delivery-admin.php
@@ -1,17 +1,11 @@
 <?php
-
 class Woo_Estimate_Delivery_Admin {
-
 	public function __construct() {}
-
-
 	public function edd_setting_page( $settings ) {
 		$settings[] = include 'class-wc-settings-estimate-delivery.php';
 		return $settings;
 	}
-
-	public function edd_simple_product()
-{
+	public function edd_simple_product(){
     $field = array(
         'id'          => 'delivery_date_simple',
         'class'       => 'show_if_simple',
@@ -22,10 +16,7 @@ class Woo_Estimate_Delivery_Admin {
         'description' => 'Add only number of days',
     );
     woocommerce_wp_text_input( $field );
-}
-	/**
-	 * Update the product meta with extra fields
-	*/
+	}
 	public function update_product_meta( $product_id ) {
 	    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
         return;
@@ -33,10 +24,8 @@ class Woo_Estimate_Delivery_Admin {
 		if ( isset( $_POST['delivery_date_simple'] ) && '' != $_POST['delivery_date_simple'] ) {
 		    update_post_meta( $product_id, 'delivery_date_simple', $_POST['delivery_date_simple'] );
 		}
-
 	}
-	public function delivery_date_variations( $loop, $variation_data, $variation )
-    {
+	public function delivery_date_variations( $loop, $variation_data, $variation ){
     woocommerce_wp_text_input( array(
         'id'          => 'delivery_variation[' . $loop . ']',
         'class'       => 'short',
@@ -46,22 +35,19 @@ class Woo_Estimate_Delivery_Admin {
         'value'       => get_post_meta( $variation->ID, 'delivery_date_var', true ),
     ) );
     }
-    public function betterwoo_save_variations( $variation_id, $i )
-    {
+    public function betterwoo_save_variations( $variation_id, $i ){
     $custom_field = $_POST['delivery_variation'][$i];
     if ( isset( $custom_field ) ) {
         update_post_meta( $variation_id, 'delivery_date_var', esc_attr( $custom_field ) );
     }
     }
-    public function better_wc_template( $template, $template_name, $template_path )
-    {
-        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'templates/woocommerce/single-product/add-to-cart/variation.php';
+    public function better_wc_template( $template, $template_name, $template_path ){
+    require_once plugin_dir_path( dirname( __FILE__ ) ) . 'templates/woocommerce/single-product/add-to-cart/variation.php';
     $template_directory = trailingslashit( plugin_dir_path( __FILE__ ) ) . 'templates/woocommerce/';
     $path = $template_directory . $template_name;
     return ( file_exists( $path ) ? $path : $template );
     }
-	public function enqueue_color_picker( $hook_suffix )
-    {
+	public function enqueue_color_picker( $hook_suffix ){
     wp_enqueue_style( 'wp-color-picker' );
     wp_enqueue_script(
         'badge-color',
@@ -72,26 +58,16 @@ class Woo_Estimate_Delivery_Admin {
     );
 	wp_enqueue_style( 'estimate-delivery-style', plugins_url( 'assets/css/estimate-delivery.css', __FILE__ ) );
     }
-
 	public function action_links( $links, $file ){
-
 		if ( 'woo-estimate-delivery/estimate-delivery.php' == $file ) {
 			$links[] = '<a href="' . admin_url( 'admin.php?page=wc-settings&tab=estimate_delivery&section' ) . '">' . esc_html__( 'Setari', 'woo-estimate-delivery' ) . '</a>';
 		}
-
 		return $links;
 	}
-
 	public function settings_links( $links ){
-
 		if ( is_array( $links ) ) {
-            $links['estimate-delivery-settings'] = sprintf('<a href="%s">%s</a>', admin_url( 'admin.php?page=wc-settings&tab=estimate-delivery' ), __( 'Settings', 'woo-estimate-delivery' ) );
+            $links['estimate-delivery-settings'] = sprintf('<a href="%s">%s</a>', admin_url( 'admin.php?page=wc-settings&tab=estimate_delivery' ), __( 'Settings', 'woo-estimate-delivery' ) );
         }
-
 		return $links;
-
 	}
-
 	}
-
-
